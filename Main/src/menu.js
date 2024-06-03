526c06ad3612ac8421e7b44d6e90e188d05ef0ac
Code-Zender <165061201+Code-Zender@users.noreply.github.com>
2024-06-03 17:21:55 +0200

    Delete Main directory

--- Main/src/menu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const menuBtn = document.querySelector('.menu-btn');
-    const menuOptions = document.querySelector('.menu-options');
-    
-    menuBtn.classList.toggle('open');
-    menuOptions.classList.toggle('open');
-  }
-  
