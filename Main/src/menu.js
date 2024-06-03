6d3589418933fefb77081fa68190e80e39ddd430
Code-Zender <165061201+Code-Zender@users.noreply.github.com>
2024-06-03 12:32:48 +0200

    Add files via upload

---
+++ Main/src/menu.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menuBtn = document.querySelector('.menu-btn');
+    const menuOptions = document.querySelector('.menu-options');
+    
+    menuBtn.classList.toggle('open');
+    menuOptions.classList.toggle('open');
+  }
+  
