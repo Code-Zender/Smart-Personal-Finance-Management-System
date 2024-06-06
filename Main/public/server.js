e6dfc5fd95f2718925a23a9207b1deb252a001c9
Code-Zender <165061201+Code-Zender@users.noreply.github.com>
2024-06-06 17:31:40 +0000

    thr

--- Main/public/server.js
+++ Main/public/server.js
@@ -15,6 +15,21 @@ app.get('/', (req, res) => {
 
 app.get('/login/register.html', (req, res) => {
   res.sendFile(__dirname + '/login/register.html');
+  
+  app.post('/submitRegistration',async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const data = [{username,password}]
+    console.log("4");
+    try {
+      const result = await db.collection('user-data').insertOne(data);
+      console.log('Daten erfolgreich gespeichert:', result.ops);
+      res.status(200).json({ message: 'Daten erfolgreich gespeichert' });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Fehler beim Speichern der Daten in der Datenbank',err });
+    }
+  })
 });
 
 
@@ -24,9 +39,7 @@ app.get('/login/register.html', (req, res) => {
 const startServer = async () => {
   try {
     console.log("2");
-    const client = new MongoClient(url, {
-      tls: true
-    });
+    const client = new MongoClient(url, {});
     console.log("3");
     console.log('Verbunden mit der MongoDB');
     const db = client.db('user'); // Datenbank auswählen
@@ -44,20 +57,7 @@ const startServer = async () => {
         res.status(500).json({ error: 'Fehler beim Speichern der Daten in der Datenbank' });
       }
     });
-    app.post('/submitRegistration',async (req, res) => {
-      const username = req.body.username;
-      const password = req.body.password;
-      const data = [{username,password}]
-      console.log("4");
-      try {
-        const result = await db.collection('user-data').insertOne(data);
-        console.log('Daten erfolgreich gespeichert:', result.ops);
-        res.status(200).json({ message: 'Daten erfolgreich gespeichert' });
-      } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: 'Fehler beim Speichern der Daten in der Datenbank' });
-      }
-    })
+
     app.listen(3000, () => console.log('Server gestartet auf http://localhost:3000'));
   } catch (err) {
     console.error('Fehler beim Verbindungsaufbau zur MongoDB:', err);
